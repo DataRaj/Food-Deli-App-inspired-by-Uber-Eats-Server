87a53132afa86307436c62beb21a4932130f6e00
dataraj <dhadas808@yahoo.com>
2024-04-19 17:43:28 +0530

    added the upload files

--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -2,33 +2,40 @@ import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { multerOptions } from 'src/config/multer.config';
-import * as AWS from 'aws-sdk';
-
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { marshall } from '@aws-sdk/util-dynamodb';
+import { Readable } from 'stream';
 @Controller('uploads')
 export class UploadsController {
-	constructor(private readonly configService: ConfigService) {}
+	constructor(private readonly configService: ConfigService) {
+		
+	}
 
 	@Post('aws')
 	@UseInterceptors(FileInterceptor('file'))
 	async uploadFileAws(@UploadedFile() file) {
 		const BUCKET_NAME = this.configService.get('BUCKET_NAME');
 
-		AWS.config.update({
+		const s3 = new S3Client({
+			region: 'your-region-here',
 			credentials: {
 				accessKeyId: this.configService.get('AWS_KEY'),
 				secretAccessKey: this.configService.get('AWS_SECRET'),
 			},
 		});
 		try {
-			const objectName = `${Date.now() + file.originalname}`;
-			await new AWS.S3()
-				.putObject({
-					Body: file.buffer,
-					Bucket: BUCKET_NAME,
-					Key: objectName,
-					ACL: 'public-read',
-				})
-				.promise();
+			const objectName = `${Date.now()}-${file.originalname}`;
+			const buffer = file.buffer;
+    const stream = Readable.from(buffer);
+
+   			 const command = new PutObjectCommand({
+     		 Bucket: BUCKET_NAME,
+			 Key: objectName,
+			 Body: stream,
+			 ContentType: file.mimetype,
+     		 ACL: 'public-read',
+    });
+			await s3.send(command);
 			const url = `https://${BUCKET_NAME}.s3.amazonaws.com/${objectName}`;
 			return { url };
 		} catch (e) {
@@ -42,3 +49,5 @@ export class UploadsController {
 		return { url: `${this.configService.get('APP_URL')}/${file.filename}` };
 	}
 }
+
+
